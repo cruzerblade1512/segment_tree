042b4cace075e1174c903c8295c00466289acce1
cruzerblade1512 <73781984+cruzerblade1512@users.noreply.github.com>
2020-12-30 14:12:12 +0530

    Add files via upload

---
+++ segtree2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+//sum query segment tree
+
+void createSeg(int *arr,int *tree,int index,int s,int e)
+{
+    //BASE CONDITION
+    //LEAF NODES
+    if(s == e)
+    {
+        tree[index] = arr[s];
+        return;
+    }
+    //EXIT CONDITION
+    if(s > e)
+        return;
+
+    int mid = (s+e)/2;
+    createSeg(arr,tree,2*index,s,mid);
+    createSeg(arr,tree,2*index + 1,mid+1,e);
+
+    int left = tree[2*index];
+    int right = tree[2*index+1];
+
+    tree[index] = left+right;
+
+
+}
+int main()
+{
+    int arr[] = {2,3,-1,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int *tree = new int[4*n+1];
+    createSeg(arr,tree,1,0,n-1);
+
+    //tree array is 1 based indexing
+
+    for(int j=1;j<4*n+1;j++)
+    {
+        cout<< tree[j] <<" ";
+    }
+    return 0;
+}
